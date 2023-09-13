b73984f06211d9982adb87e33b2406c6aca43b63
MinhVo <minhvo4398@gmail.com>
2023-09-13 15:20:54 +0700

    involke command

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,8 +1,8 @@
 //// <reference type = "cypress" />
 describe("Our first suite", () => {
 
- 
-    it.skip("first test", () => {
+
+    it("first test", () => {
         cy.visit('/pages')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -40,7 +40,7 @@ describe("Our first suite", () => {
 
     })
 
-    it('second test',()=>{
+    it('second test', () => {
         cy.visit('/pages')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -49,29 +49,88 @@ describe("Our first suite", () => {
 
         cy.contains('Sign in')
 
-        cy.contains('[status="warning"]','Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
 
         cy.get('#inputEmail3')
-        .parents('form')
-        .find('button')
-        .should('contain','Sign in')
-        .parents('form')
-        .find('nb-checkbox')
-        .click()
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
 
-        cy.contains('nb-card','Horizontal form').find('[type="email"]')
+        cy.contains('nb-card', 'Horizontal form').find('[type="email"]')
 
     });
 
-    it.only('then and wrap method', ()=>{
+    it('then and wrap method', () => {
         cy.visit('/pages')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
 
-        cy.contains('nb-card','Using the Grid').find('[for="inputEmail1"]').should('contain','Email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+        // cypress style
+        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email') // chai assertion
+            expect(passwordLabelFirst).to.equal('Password')
+
+            cy.contains('nb-card', 'Basic form').then(secondForm => {
+                const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst).to.equal(passwordSecondText)
+
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+            })
+        })
+
+    })
+
+    it('invoke command', () => {
+        cy.visit('/pages')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        //2 
+        cy.get('[for="exampleInputEmail1"]').then(label => {
+            expect(label.text()).to.equal("Email address")
+        })
+
+        //3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+            expect(text).to.equal('Email address')
+        })
+
+        cy.contains('nb-card', 'Basic form')
+            .find('nb-checkbox')
+            .click()
+            .find('.custom-checkbox')
+            .invoke('attr', 'class')
+            // .should('contain','check')
+            .then(classValue => {
+                expect(classValue).contain('checked')
+            })
+    })
+
+    it.only('assert property',()=>{
+        cy.visit('/pages')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card','Common Datepicker').find('input').then(input =>{
+                cy.wrap(input).click()
+                cy.get('nb-calendar-day-picker').contains('13').click()
+                cy.wrap(input).invoke('prop','value').should('contain','Sep 13, 2023')
+        })
     })
 
 
-    
 
 })
